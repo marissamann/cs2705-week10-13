4de9bc1be4ef52a80103c6a82303839470abeb2c
marissamann <marissamann@mail.weber.edu>
2022-03-22 19:44:20 -0600

    form not functioning

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,3 +5,9 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 //TODO - Your ES6 JavaScript code (if any) goes here
 import 'bootstrap'
+import { addGuest } from './week10'
+
+alert("Hi")
+//inspect -> console to see message
+console.log("Hello Console")
+document.querySelector("#guestForm").onsubmit = addGuest
---
+++ src/javascripts/week10.js
@@ -0,0 +1,30 @@
+let guests = []
+
+export function addGuest(event) {
+    event.preventDefault()
+    //^^^do not submit anywhere
+
+    //object creation
+    let guest = {
+        fname: document.getElementById("fn").value,
+        //query selector method is more modern
+        lname: document.querySelector("#ln").value
+    }
+
+    //.trim removes spaces before and after text
+    //empty string is false automatically
+    if(guest.fname.trim() && guest.lname.trim()) {
+        guests.push(guest)
+        let row = document.createElement("tr")
+        //add to row element
+        //instead of ' and " ... ` ` creates multiline string
+        row.innerHTML = `
+            <td>${guest.fname}</td>
+            <td>${guest.lname}</td>
+        `
+        //append child adds a CHILD to TBODY(first one if no id specification)
+        document.querySelector("tbody").appendChild("row")
+    }
+    this.reset()
+    
+}
